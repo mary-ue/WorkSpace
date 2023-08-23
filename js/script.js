c4e5545874ab5dbb84263294d106ee09fa7d5f48
mary-ue <maryandreeva100@gmail.com>
2023-08-23 17:46:23 +0300

    Add Cities in Select

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
-const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false, // отключить поиск
-  itemSelectText: '', // убрать надпись внутри
-});
+// Api
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+// 
+const getData = async(url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+}
+
+// Start
+const init = () => {
+  // Choices JS
+  const citySelect = document.querySelector('#city');
+  const cityChoices = new Choices(citySelect, {
+    searchEnabled: false, // отключить поиск
+    itemSelectText: '', // убрать надпись внутри
+  });
+
+  // Cities
+  getData(`${API_URL}${LOCATION_URL}`, (locationData) => {
+    const locations = locationData.map((location) => ({
+      value: location,
+    }));
+    cityChoices.setChoices(locations, "value", "label", true)
+  }, (err) => {
+    console.log(err);
+  })
+}
+
+init();
+
+
+
+
+
